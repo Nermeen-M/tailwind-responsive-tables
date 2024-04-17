e8be712f6fdc71fc98f939b1cc79f98a9f0c858e
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2024-04-17 11:35:59 +0200

    Refactor theme toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,17 @@
-import { useEffect, useState } from "react";
-
 import users from "./data/users.json";
 import UserTable from "./components/UserTable";
 import UserList from "./components/UserList";
+import ThemeToggler from "./components/ThemeToggler";
 
 export default function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-
-  useEffect(() => {
-    {
-      isDarkTheme
-        ? document.body.classList.add("dark")
-        : document.body.classList.remove("dark");
-    }
-  }, [isDarkTheme]);
-
   return (
     <div className="w-full h-full min-h-screen p-6 bg-background">
-      <div>
-        <button
-          onClick={() => {
-            setIsDarkTheme(true);
-          }}
-        >
-          Dark
-        </button>
-        <button
-          onClick={() => {
-            setIsDarkTheme(false);
-          }}
-        >
-          Light
-        </button>
-      </div>
+      <ThemeToggler />
+
       <h1 className="text-xl font-bold uppercase text-center text-text m-8">
         Users
       </h1>
+
       <div className="mb-8">
         <h2 className="text-lg font-bold text-primary mb-4">Using table</h2>
         <UserTable users={users} />
